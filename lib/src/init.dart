77815ec08d7fc8add3a7b826ebd3650ab263c0fe
Kleak <me@kleak.dev>
2020-11-28 22:23:44 +0100

    give the possibility to use another path

--- lib/src/init.dart
+++ lib/src/init.dart
@@ -2,6 +2,6 @@ import 'dart:ffi';
 
 import 'package:gtk/src/gtk.dart';
 
-void initGtk() {
-  gtk = DynamicLibrary.open('/usr/lib/x86_64-linux-gnu/libgtk-3.so');
+void initGtk({String path = '/usr/lib/x86_64-linux-gnu/libgtk-3.so'}) {
+  gtk = DynamicLibrary.open(path);
 }
